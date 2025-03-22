37a5167b5a285bf4e3123cf0db3862533a1b298e
Valerie Vozza <valerievozza@gmail.com>
2025-03-21 22:58:30 -0700

    update server and scripts

--- src/server.js
+++ src/server.js
@@ -1,38 +1,52 @@
 import express, { json } from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { MongoClient } from 'mongodb';
-import cors from 'cors';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
 dotenv.config();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Allow cross-origin requests
 app.use(cors());
-app.use(json());
+app.use(express.json());
 
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, {
-  useUnifiedTopology: true
-});
-
+// MongoDB setup
+const client = new MongoClient(process.env.MONGODB_URI);
 await client.connect().catch((err) => {
   console.error(`MongoDB connection error: ${err}`);
   process.exit(1);
 })
 
-const collection = client.db("vegan-pdx").collection("restaurants");
+const db = client.db("vegan-pdx");
+const restaurants = db.collection("restaurants");
 
 // Example GET endpoint to fetch data
 app.get('/api/restaurants', async (req, res) => {
   try {
-    const results = await collection.find({}).toArray();
+    const results = await restaurants.find({}).toArray();
     console.log('results', results)
     res.json(results);
   } catch (error) {
     res.status(500).send(error.message);
   }
 });
+
+// Serve frontend static files in production
+if (process.env.NODE_ENV === 'production') {
+  const clientDist = path.join(__dirname, '..', 'dist');
+
+  app.use(express.static(clientDist));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(clientDist, 'index.html'));
+  })
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);

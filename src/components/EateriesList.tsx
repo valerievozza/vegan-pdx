7204a06c4b72f0d836fcd63518d1f972e9583f38
Valerie Vozza <valerievozza@gmail.com>
2025-03-27 21:55:24 -0700

    add loader to mobile list page

--- src/components/EateriesList.tsx
+++ src/components/EateriesList.tsx
@@ -1,4 +1,6 @@
-import { Stack } from '@mui/material';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Stack from '@mui/material/Stack';
 import EateryCard from './EateryCard';
 import type { Eatery } from '../models/Eatery';
 
@@ -8,10 +10,20 @@ type Props = {
 
 export default function EateriesList({ eateries }: Props) {
   return (
-    <Stack spacing={2}>
-      {eateries.map((eatery) => (
-        <EateryCard key={eatery._id} eatery={eatery} />
-      ))}
-    </Stack>
+    <>
+      <Stack spacing={2}>
+        {eateries.map((eatery) => (
+          <EateryCard key={eatery._id} eatery={eatery} />
+        ))}
+      </Stack>
+      {!eateries.length &&
+        <Backdrop
+          open={!eateries.length}
+          sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      }
+    </>
   )
 }
